2be51a3290d9ff3b9fc44ee6ec0083e2eec56761
Light2288 <davide.aliti@gmail.com>
2025-09-30 03:16:59 +0200

    Next.js 15: fix static export for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -54,7 +54,7 @@ const securityHeaders = [
   },
 ]
 
-const output = process.env.EXPORT ? 'export' : undefined
+const output = 'export' // always generate static export
 const basePath = process.env.BASE_PATH || undefined
 const unoptimized = process.env.UNOPTIMIZED ? true : undefined
 

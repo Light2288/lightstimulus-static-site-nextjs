b2b26816b764ce333deb3416e25811054e2713fa
Light2288 <davide.aliti@gmail.com>
2025-11-18 00:42:27 +0100

    feat(hero): add initial version of Taglines component

---
+++ components/home/Taglines.tsx
@@ -0,0 +1,8 @@
+export default function Taglines() {
+  return (
+    <section className="mt-6 flex flex-col items-center px-4 text-center text-lg text-gray-600 dark:text-gray-300">
+      {/* Tagline animations will be implemented later */}
+      <p>Where light meets computation.</p>
+    </section>
+  )
+}

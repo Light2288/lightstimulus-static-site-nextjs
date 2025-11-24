694ea19a0205fdddfe57cca45918adcb079e24ef
Light2288 <davide.aliti@gmail.com>
2025-11-24 02:23:56 +0100

    feat(hero): fix seo and site metadata

--- app/seo.tsx
+++ app/seo.tsx
@@ -1,31 +1,42 @@
 import { Metadata } from 'next'
 import siteMetadata from '@/data/siteMetadata'
 
-interface PageSEOProps {
+interface PageSEOProps extends Partial<Metadata> {
   title: string
   description?: string
   image?: string
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any
+  slug?: string
 }
 
-export function genPageMetadata({ title, description, image, ...rest }: PageSEOProps): Metadata {
+export function genPageMetadata({
+  title,
+  description,
+  image,
+  slug,
+  ...rest
+}: PageSEOProps): Metadata {
+  const fullUrl = slug ? `${siteMetadata.siteUrl}/${slug}` : siteMetadata.siteUrl
+
+  const ogImage = image
+    ? `${siteMetadata.siteUrl}${image}`
+    : `${siteMetadata.siteUrl}${siteMetadata.socialBanner}`
+
   return {
     title,
     description: description || siteMetadata.description,
     openGraph: {
       title: `${title} | ${siteMetadata.title}`,
       description: description || siteMetadata.description,
-      url: './',
+      url: fullUrl,
       siteName: siteMetadata.title,
-      images: image ? [image] : [siteMetadata.socialBanner],
+      images: [ogImage],
       locale: 'en_US',
       type: 'website',
     },
     twitter: {
       title: `${title} | ${siteMetadata.title}`,
       card: 'summary_large_image',
-      images: image ? [image] : [siteMetadata.socialBanner],
+      images: [ogImage],
     },
     ...rest,
   }

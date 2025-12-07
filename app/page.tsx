7b7e546ff58556c4ffea6c058f6cd3696d65e9e1
Light2288 <davide.aliti@gmail.com>
2025-12-08 00:40:07 +0100

    feat(taglines): add FixedAnalogyParagraph to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@ import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 import Hero from '@/components/home/hero/Hero'
 import Taglines from '@/components/home/Taglines'
+import FixedAnalogyParagraph from '@/components/home/FixedAnalogyParagraph'
 
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
+
   return (
     <>
       <Hero />
       <Taglines />
+      <FixedAnalogyParagraph />
       <Main posts={posts} />
     </>
   )

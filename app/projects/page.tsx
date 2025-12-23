34200c22a73deac0ba838d7a6f942e8d38ff39bb
Light2288 <davide.aliti@gmail.com>
2025-12-24 00:05:11 +0100

    add suspense to /projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,16 @@
 import { allProjects } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
 import ProjectsListLayoutWithTags from '@/layouts/ProjectsListLayoutWithTags'
+import { Suspense } from 'react'
 
 export const metadata = genPageMetadata({ title: 'Projects' })
 
 export default function ProjectsPage() {
   const projects = [...allProjects].sort((a, b) => +new Date(b.date) - +new Date(a.date))
 
-  return <ProjectsListLayoutWithTags projects={projects} />
+  return (
+    <Suspense fallback={null}>
+      <ProjectsListLayoutWithTags projects={projects} />
+    </Suspense>
+  )
 }

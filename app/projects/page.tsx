e244d549160ca06492e2281f826c44372b8215ed
Light2288 <davide.aliti@gmail.com>
2025-12-23 11:19:30 +0100

    feat(projects): add tag-filtered projects list layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,11 @@
-// app/projects/page.tsx
 import { allProjects } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
-import ProjectsGrid from '@/components/projects/ProjectsGrid'
+import ProjectsListLayoutWithTags from '@/layouts/ProjectsListLayoutWithTags'
 
 export const metadata = genPageMetadata({ title: 'Projects' })
 
 export default function ProjectsPage() {
   const projects = [...allProjects].sort((a, b) => +new Date(b.date) - +new Date(a.date))
 
-  return <ProjectsGrid projects={projects} />
+  return <ProjectsListLayoutWithTags projects={projects} />
 }

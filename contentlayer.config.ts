7f7199a5b6da75a02784b379e39497f3e3d2651e
Light2288 <davide.aliti@gmail.com>
2026-01-08 01:02:25 +0100

    add structuredData to Blog in contentlayer.config.ts

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -128,6 +128,19 @@ export const Blog = defineDocumentType(() => ({
       type: 'string',
       resolve: (doc) => doc.summary?.it ?? '',
     },
+    structuredData: {
+      type: 'json',
+      resolve: (doc) => ({
+        '@context': 'https://schema.org',
+        '@type': 'BlogPosting',
+        headline: doc.title,
+        datePublished: doc.date,
+        dateModified: doc.lastmod || doc.date,
+        description: doc.summary,
+        image: doc.images ? doc.images[0] : siteMetadata.socialBanner,
+        url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`,
+      }),
+    },
   },
 }))
 
